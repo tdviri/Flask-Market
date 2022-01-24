cd1d2cb9fe3f4a51ed0ed3ec86dafa03cc292586
tdviri <tdviri20@gmail.com>
2022-01-24 13:51:09 -0500

    implemented password hashing

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '1f8e53160d65e91e9439ecb3'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from market import routes
+
